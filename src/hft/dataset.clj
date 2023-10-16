f72c05ea680770f0b5a5d501c508c80a5de3461a
Oleh Kylymnyk <okilimnik@gmail.com>
2023-10-16 12:15:27 +0200

    transients DONE

--- src/hft/dataset.clj
+++ src/hft/dataset.clj
@@ -2,7 +2,6 @@
   (:require [clojure.java.io :as io]
             [clojure.set :as set]
             [clojure.math :as math]
-            [clojure.core.async :refer [thread]]
             [taoensso.timbre :as log]
             [mikera.image.core :as i]
             [mikera.image.colours :as c]
@@ -39,62 +38,77 @@
     (i/set-pixels image pixels)
     image))
 
-(defn add-price-level [col k min-price level-shift]
-  (assoc! col k (assoc! (get col k) :price-level (int (/ (- k min-price) level-shift)))))
+(defn add-price-level [m min-price level-shift]
+  (persistent!
+   (reduce (fn [out k]
+             (let [v (get out k)]
+               (assoc! out k (assoc v :price-level (int (/ (- k min-price) level-shift))))))
+           (transient m)
+           (.keySet m))))
 
 (def PRICE-LEVELS
   (->> (for [i (range INPUT-SIZE)]
          [i 0])
        (into {})))
 
-(defn calc-quantities-by-price-level [col]
-  (reduce (fn [res entry]
-            (let [v (val entry)]
-              (update res (:price-level v) + (:qty v))))
-          PRICE-LEVELS
-          col))
+(defn calc-quantities-by-price-level [m]
+  (persistent!
+   (reduce (fn [out k]
+             (let [v (get m k)
+                   level (:price-level v)]
+               (assoc! out level (+ (get out level) (:qty v)))))
+           (transient PRICE-LEVELS)
+           (.keySet m))))
 
-(defn low-qty? [price quantities-by-price-level]
-  (let [v (val price)
-        qty (get quantities-by-price-level (:price-level v))]
-    (< qty 0.5)))
+(defn remove-low-qty [m quantities-by-price-level]
+  (persistent!
+   (reduce (fn [out k]
+             (let [v (get m k)
+                   level (:price-level v)
+                   qty (get quantities-by-price-level level)]
+               (if (< qty 0.5)
+                 (dissoc! out k)
+                 out)))
+           (transient m)
+           (.keySet m))))
 
 (defn denoise
   "This function takes too long, like 4 seconds."
   [series max-price min-price]
   (let [shift (- max-price min-price)
         level-shift (/ shift INPUT-SIZE)
-        _ (doseq [s series]
-            (doseq [b (keys (:bids s))]
-              (add-price-level (:bids s) b min-price level-shift))
-            (doseq [a (keys (:asks s))]
-              (add-price-level (:asks s) a min-price level-shift)))
+        enriched-series (map (fn [s]
+                               (-> s
+                                   (update :bids #(add-price-level % min-price level-shift))
+                                   (update :asks #(add-price-level % min-price level-shift))))
+                             series)
         bid-quantities-by-price-level (map (fn [s]
                                              (calc-quantities-by-price-level (:bids s)))
-                                           series)
+                                           enriched-series)
         ask-quantities-by-price-level (map (fn [s]
                                              (calc-quantities-by-price-level (:asks s)))
-                                           series)
-        _ (doseq [[idx s] (map-indexed vector series)]
-            (doseq [b (keys (:bids s))]
-              (when (low-qty? b (nth bid-quantities-by-price-level idx))
-                (dissoc! (:bids s) b)))
-            (doseq [a (keys (:asks s))]
-              (when (low-qty? a (nth ask-quantities-by-price-level idx))
-                (dissoc! (:asks s) a))))
-        all-prices (map first (mapcat #(concat (:bids %) (:asks %)) series))
+                                           enriched-series)
+        filtered-series (map-indexed
+                         (fn [idx s]
+                           (-> s
+                               (update :bids #(remove-low-qty % (nth bid-quantities-by-price-level idx)))
+                               (update :asks #(remove-low-qty % (nth ask-quantities-by-price-level idx)))))
+                         enriched-series)
+        all-prices (concat (mapcat (comp keys :bids) series)
+                           (mapcat (comp keys :asks) series))
         new-max-price (+ (apply max all-prices) 0.000001)
         new-min-price (apply min all-prices)]
     (if (and (= new-max-price max-price)
              (= new-min-price min-price))
       [bid-quantities-by-price-level ask-quantities-by-price-level]
-      (recur series new-max-price new-min-price))))
+      (recur filtered-series new-max-price new-min-price))))
 
-(defn create-input-image [order-book-series]
-  (let [all-prices (map first (mapcat #(concat (:bids %) (:asks %)) order-book-series))
+(defn create-input-image [series]
+  (let [all-prices (concat (mapcat (comp keys :bids) series)
+                           (mapcat (comp keys :asks) series))
         max-price (+ (apply max all-prices) 0.000001)
         min-price (apply min all-prices)
-        [bid-qties ask-qties] (time (denoise order-book-series max-price min-price))
+        [bid-qties ask-qties] (time (denoise series max-price min-price))
         image (->image bid-qties ask-qties INPUT-SIZE INPUT-SIZE)
         dir (io/file "./dataset")]
     (when-not (.exists dir)
@@ -113,16 +127,21 @@
                                             [(parse-double price) {:qty (parse-double qty)}]))
                                     (into {}))))))
 
-(defn filter-pos-qty [price-map]
-  (->> price-map
-       (filter (comp pos? :qty val))
-       (into {})))
+(defn filter-pos-qty [m]
+  (persistent!
+   (reduce (fn [out k]
+             (let [v (get out k)]
+               (if (pos? (:qty v))
+                 out
+                 (dissoc! out k))))
+           (transient m)
+           (.keySet m))))
 
 (defn add-to-states [current-states event]
   (let [last-state (last current-states)]
     (as-> current-states $
       (conj $ (-> last-state
-                  (assoc :lastUpdateId (:finalUpdateId event))
+                  (assoc :lastUpdateId (:lastUpdateId event))
                   (update :asks merge (:asks event))
                   (update :asks filter-pos-qty)
                   (update :bids merge (:bids event))
@@ -135,17 +154,20 @@
   (when (empty? @states)
     (swap! states conj (-> (api/depth! SYMBOL)
                            mapify-prices)))
-  (when (> (:finalUpdateId event) (:lastUpdateId (last @states)))
+  (when (> (:lastUpdateId event) (:lastUpdateId (last @states)))
     (swap! states #(add-to-states % event)))
   (when (= (count @states) STATES-MAX-SIZE)
-    (let [data (mapv #(-> %
-                          (update :bids seq)
-                          (update :asks seq))
-                     @states)]
-      (create-input-image data))))
+    (create-input-image @states)))
 
 (defn stop-preparation! []
   (.closeConnection @api/ws-client @api-stream-id))
+
+(defn event->readable-event [event]
+  (-> event
+      (set/rename-keys {:u :lastUpdateId
+                        :b :bids
+                        :a :asks})
+      mapify-prices))
 
 (def on-order-book-change
   (reify WebSocketMessageCallback
@@ -153,11 +175,7 @@
       (let [event (parse-string json true)]
         (if (= (:e event) "depthUpdate")
           (try
-            (calc-new-state (-> event
-                                (set/rename-keys {:u :finalUpdateId
-                                                  :b :bids
-                                                  :a :asks})
-                                mapify-prices))
+            (calc-new-state (event->readable-event event))
             (catch Exception e
               (stop-preparation!)
               (prn e)
