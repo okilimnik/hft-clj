7f9cca2e63a53a0e63aa0295da1df6dd6ae4e49c
Oleh Kylymnyk <okilimnik@gmail.com>
2023-10-19 18:33:20 +0200

    refactor

--- src/hft/gcloud.clj
+++ src/hft/gcloud.clj
@@ -4,14 +4,16 @@
 
 (def storage (atom nil))
 (def bucket-name "neusa-datasets")
+(def folder (atom nil))
 
 (defn init! []
-  (reset! storage (.getService (StorageOptions/getDefaultInstance))))
+  (reset! storage (.getService (StorageOptions/getDefaultInstance)))
+  (reset! folder (str (System/currentTimeMillis))))
 
 (defn upload-file! [filename filepath]
   (try
     (when-not @storage (init!))
-    (let [blob-id (BlobId/of bucket-name (str "order_book/" filename))
+    (let [blob-id (BlobId/of bucket-name (str "order_book2/" @folder "/" filename))
           blob-info (.build (BlobInfo/newBuilder blob-id))]
       (.createFrom @storage blob-info (io/input-stream (io/file filepath)) (into-array Storage$BlobWriteOption [])))
     (catch Exception e (prn e))))
