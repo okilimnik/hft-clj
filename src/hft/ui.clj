84d15c6c27ed4ec25228b0f9b37d385cffbb6184
Oleh Kylymnyk <okilimnik@gmail.com>
2024-08-11 22:28:01 +0200

    commented ui

--- src/hft/ui.clj
+++ src/hft/ui.clj
@@ -1,15 +1,15 @@
 (ns hft.ui
-  (:require [clojure.java.io :as io]
+  #_(:require [clojure.java.io :as io]
             [clojure.core.async :refer [thread]]
             [membrane.ui :as ui]
             [membrane.java2d :as backend]
             [hft.dataset :as dataset]))
 
-(def state-atm
+#_(def state-atm
   (atom
    {:src "file" :label "None"}))
 
-(defn root [{:keys [label src]}]
+#_(defn root [{:keys [label src]}]
   (if (.exists (io/file src))
     (ui/vertical-layout
      (ui/label label)
@@ -17,8 +17,8 @@
     (ui/label "Warming...")))
 
 (defn start! []
-  (thread
+  #_(thread
     (dataset/pipeline-v1 {:on-update #(reset! state-atm %)
                           :ui? true}))
-  (backend/run #(root @state-atm)))
+  #_(backend/run #(root @state-atm)))
   
