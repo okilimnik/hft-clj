7cba3b3b8db5889187b2f79b9096b7d6cddb74b7
Oleh Kylymnyk <okilimnik@gmail.com>
2024-08-16 17:37:44 +0200

    added logs

--- src/hft/dataset.clj
+++ src/hft/dataset.clj
@@ -71,8 +71,9 @@
                                                        (> bid-qty-change-ratio 2))
                                       ask-signal? (and (> max-ask-distance 2)
                                                        (> ask-qty-change-ratio 2))]
-                                  (prn "bid-qty-change-ratio: " bid-qty-change-ratio)
-                                  (prn "ask-qty-change-ratio: " ask-qty-change-ratio)
+                                  (when (or bid-signal? ask-signal?)
+                                    (prn "bid-qty-change-ratio: " bid-qty-change-ratio)
+                                    (prn "ask-qty-change-ratio: " ask-qty-change-ratio))
                                   (if (and bid-signal? ask-signal?)
                                     (> (abs (- max-bid-distance max-ask-distance)) 2)
                                     (or bid-signal? ask-signal?))))
