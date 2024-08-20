f8f6c96c29dbe2f80c4ee9187d0bf4090309f167
Oleh Kylymnyk <okilimnik@gmail.com>
2024-08-21 00:21:47 +0200

    simplify things

--- scripts/download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#cd dataset
-#gsutil -m cp -r "gs://neusa-a919b.appspot.com/order_book_11_08_2024" .
--- test/hft/dataset_test.clj
+++
@@ -1,6 +0,0 @@
-(ns hft.dataset-test
-  (:require [clojure.test :refer [deftest is]]
-            [hft.dataset :as sut]))
-
-(deftest sort-levels-by-qty-test
-  (is (= [6 7 8] (sut/get-levels-with-max-qty-sorted [0 0 0 0 0 0 0.5 0.4 0.4 0.3]))))
