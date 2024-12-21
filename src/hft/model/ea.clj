dda019207ed5ed3a334c2f85b3cde261d934e9b2
Oleh Kylymnyk <okilimnik@gmail.com>
2024-12-21 20:46:18 +0100

    wip GA

--- src/hft/model/ea.clj
+++ src/hft/model/ea.clj
@@ -1,20 +1,33 @@
 (ns hft.model.ea
-  (:require [hft.interop :refer [as-function]])
-  (:import [io.jenetics BitChromosome Genotype]
-           [io.jenetics.engine Engine EvolutionResult]))
+  (:require
+   [hft.interop :refer [as-function]])
+  (:import
+   [io.jenetics Genotype LongChromosome LongGene]
+   [io.jenetics.engine Engine EvolutionResult]))
+
+(def GENERATIONS 200)
 
 (defn eval! [gt]
-  (-> gt
-      (.chromosome)
-      (.as BitChromosome)
-      (.bitCount)))
+  (reduce (fn [res idx]
+            (let [v (-> (.get gt idx)
+                        (.as LongChromosome)
+                        (.toArray)
+                        (#(reduce + 0 %)))]
+              (+ res v)))
+          (range (.length gt))))
+
+(def timeframe-chromosome (LongGene/of 0 10))
+(def price-change-chromosome (LongGene/of 0 50))
 
 (defn start []
-  (let [gtf (Genotype/of [(BitChromosome/of 10 0.5)])
+  (let [gtf (Genotype/of (->> [(LongChromosome/of [timeframe-chromosome])
+                               (LongChromosome/of [price-change-chromosome])]
+                              (repeat 5)
+                              (mapcat identity)))
         engine (.build (Engine/builder (as-function eval!) gtf))
         result (-> engine
                    (.stream)
-                   (.limit 100)
+                   (.limit GENERATIONS)
                    (.collect (EvolutionResult/toBestGenotype)))]
     (println result)))
 
